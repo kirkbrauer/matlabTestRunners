a0ddacccdf53d18d701e6c23f8d3aee9cf45c1cd
Sam-Reinsel <71461485+SamReinsel@users.noreply.github.com>
2023-08-23 09:55:20 -0400

    Update nestedTestClass.m

--- tests/nestedTestClass.m
+++ tests/nestedTestClass.m
@@ -14,7 +14,13 @@ classdef nestedTestClass < matlab.unittest.TestCase
         function unimplementedTest(testCase)
             expectedresult = 3;
             testData = nestedtest(1,2);
-            testCase.verifyEqual(testData,expectedresult)
+            testCase.verifyEqual(testData,expectedresult);
+        end
+
+        function intTest(testCase)
+            expected = 0x10;
+            result = nestedTest(0x0f,0x01);
+            testCase.verifyEqual(result,expected);
         end
     end
     
